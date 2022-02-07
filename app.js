a3b93be2ca36400e147486d964f5f9dc012596a7
Flyspeck101 <74536088+Flyspeck101@users.noreply.github.com>
2022-02-07 21:10:59 +1100

    Update app.js

--- app.js
+++ app.js
@@ -24,6 +24,47 @@ const Piece = {
     blackKing: 14,
 };
 
+function isSlidingPiece(piece) {
+    return piece in {3: "", 4: "", 5: "", 11: "", 12: "", 13: ""};
+}
+
+const Colour = {
+    White: 0,
+    Black: 8,
+    Invalid: -1
+};
+
+const Direction = {
+    North: 0,
+    Northeast: 1, 
+    East: 2,
+    Southeast: 3, 
+    South: 4, 
+    Southwest: 5, 
+    West: 6, 
+    Northwest: 7, 
+
+    0: "North", 
+    1: "Northeast", 
+    2: "East", 
+    3: "Southeast", 
+    4: "South", 
+    5: "Southwest", 
+    6: "West", 
+    7: "Northwest"
+};
+
+const DirectionOffset = {
+    North: -8,
+    Northeast: -7, 
+    East: +1,
+    Southeast: +9, 
+    South: +8, 
+    Southwest: +7, 
+    West: -1, 
+    Northwest: -9 
+};
+
 let castlingRights = {
     white: {
         kingside: true,
@@ -33,6 +74,31 @@ let castlingRights = {
         kingside: true,
         queenside: true
     }
+}
+
+function squaresToEdge(square, direction) {
+    let file = square % 8;
+    let rank = 8 - (square - square % 8) / 8;
+
+    const numNorth = 7 - rank; 
+    const numEast = 7 - file; 
+    const numSouth = rank; 
+    const numWest = file; 
+
+    return (() => {
+        switch (direction) {
+            case Direction.North: return numNorth;
+            case Direction.Northeast: return Math.min(numNorth, numEast);
+            case Direction.East: return numEast;
+            case Direction.Southeast: return Math.min(numSouth, numEast);
+            case Direction.South: return numSouth; 
+            case Direction.Southwest: return Math.min(numSouth, numWest);
+            case Direction.West: return numWest;
+            case Direction.Northwest: return Math.min(numNorth, numWest);
+            default: break; 
+        }
+        return 0;
+    })();
 }
 
 let enPassant = 0;
@@ -128,8 +194,52 @@ function move(from, to) {
     return capturedPiece;
 }
 
+function colour(piece) {
+    if (piece == Piece.None) return Colour.Invalid;
+    return piece - (piece % 8);
+}
+
+function generateSlidingMoves(square, piece) {
+    let allMoves = [];
+
+    const increment = (piece % 8) == 5 ? 1 : 2;
+    const start = (piece % 8) == 3 ? 1 : 0;
+
+    for (let directionIndex = start; directionIndex < 8; directionIndex += increment) {
+        for (var i = 0; i < squaresToEdge(square, directionIndex); i++) {
+            let target = square + DirectionOffset[Direction[directionIndex]] * (n + 1);
+            let targetPiece = board[target];
+
+            if (colour(targetPiece) == colour(piece)) break; 
+
+            allMoves.push({"from": square, "to": target});
+
+            if (colour(targetPiece) != colour(piece) && colour(targetPiece) != Colour.Invalid) break; 
+        }
+    }
+}
+
+function moves(colour) { // TODO: Non-sliding moves (King, Pawn, Knight)
+    let allMoves = []
+    if (colour = Colour.White) {
+        let pieces = [];
+        for (var i = board.length - 1; i >= 0; i--) {
+            if (colour(board[i]) == Colour.White) pieces.push(i);
+        }
+
+        for (var i = 0; i < pieces.length; i++) {
+            if (isSlidingPiece(board[pieces[i]])) {
+                allMoves = allmoves.concat(generateSlidingMoves(pieces[i], board[pieces[i]]));
+            }
+        }
+    } else if (colour = Colour.Black) {
+
+    }
+    return allMoves;
+} 
+
 function readFEN(fen) {
-    let [position, playing, castling, enPassantSquare, halfMoves, fullMoves] = fen.split(" ");
+    let [position, playing, castling, enPassantSquare, ...rest] = fen.split(" ");
 
     // Position 
     let positionSplit = position.split("/");
