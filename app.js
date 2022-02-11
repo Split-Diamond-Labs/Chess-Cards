04db693f1920dd0a28ddd1cd59a39091a149cfa8
Flyspeck101 <74536088+Flyspeck101@users.noreply.github.com>
2022-02-12 10:31:28 +1100

    Update app.js

--- app.js
+++ app.js
@@ -55,14 +55,14 @@ const Direction = {
 };
 
 const DirectionOffset = {
-    North: -8,
-    Northeast: -7, 
-    East: +1,
-    Southeast: +9, 
-    South: +8, 
-    Southwest: +7, 
-    West: -1, 
-    Northwest: -9 
+    "North": -8,
+    "Northeast": -7, 
+    "East": +1,
+    "Southeast": +9, 
+    "South": +8, 
+    "Southwest": +7, 
+    "West": -1, 
+    "Northwest": -9 
 };
 
 let castlingRights = {
@@ -76,9 +76,14 @@ let castlingRights = {
     }
 }
 
+let inCheck = {
+    White: false, 
+    Black: false
+}
+
 function squaresToEdge(square, direction) {
     let file = square % 8;
-    let rank = 8 - (square - square % 8) / 8;
+    let rank = 8 - (square - square % 8) / 8 - 1;
 
     const numNorth = 7 - rank; 
     const numEast = 7 - file; 
@@ -158,7 +163,7 @@ function updateBoard() {
                     text = "<img src=\"./pieces/whiteQueen.png\">";
                     break;
                 case Piece.whiteKing:
-                    text = "<img src=\"./pieces/whiteKing.png\">";
+                    text = inCheck.White ? "<img src=\"./pieces/whiteKingChecked.png\">" : "<img src=\"./pieces/whiteKing.png\">";
                     break;
 
                 case Piece.blackPawn:
@@ -177,7 +182,7 @@ function updateBoard() {
                     text = "<img src=\"./pieces/blackQueen.png\">";
                     break;
                 case Piece.blackKing:
-                    text = "<img src=\"./pieces/blackKing.png\">";
+                    text = inCheck.Black ? "<img src=\"./pieces/blackKingChecked.png\">" : "<img src=\"./pieces/blackKing.png\">";
                     break;
             }
             return text;
@@ -185,16 +190,7 @@ function updateBoard() {
     }
 }
 
-function move(from, to) {
-    let movedPiece = board[Square(from)];
-    let capturedPiece = board[Square(to)];
-    board[Square(to)] = movedPiece;
-    board[Square(from)] = Piece.None;
-    updateBoard();
-    return capturedPiece;
-}
-
-function colour(piece) {
+function getColour(piece) {
     if (piece == Piece.None) return Colour.Invalid;
     return piece - (piece % 8);
 }
@@ -207,33 +203,183 @@ function generateSlidingMoves(square, piece) {
 
     for (let directionIndex = start; directionIndex < 8; directionIndex += increment) {
         for (var i = 0; i < squaresToEdge(square, directionIndex); i++) {
-            let target = square + DirectionOffset[Direction[directionIndex]] * (n + 1);
+            let target = square + DirectionOffset[Direction[directionIndex]] * (i + 1);
             let targetPiece = board[target];
 
-            if (colour(targetPiece) == colour(piece)) break; 
+            if (getColour(targetPiece) == getColour(piece)) break; 
 
             allMoves.push({"from": square, "to": target});
 
-            if (colour(targetPiece) != colour(piece) && colour(targetPiece) != Colour.Invalid) break; 
+            if (getColour(targetPiece) != getColour(piece) && getColour(targetPiece) != Colour.Invalid) break; 
         }
     }
+
+    return allMoves;
+}
+
+function generateKingMoves(square) {
+    let possibleMoves = [];
+
+    console.log(possibleMoves);
+
+    for (var direction = 0; direction < 8; direction++) {
+        console.log(direction);
+        console.log(possibleMoves[direction]);
+        if ((square + DirectionOffset[Direction[direction]] >= 0 && square + DirectionOffset[Direction[direction]] <= 63) && getColour(board[square + DirectionOffset[Direction[direction]]]) == getColour(board[square])) {
+            continue;
+        }
+
+        if (squaresToEdge(square, direction) == 0) {
+            continue;
+        }
+
+        possibleMoves.push({from: square, to: square + DirectionOffset[Direction[direction]]});
+    }
+
+    return possibleMoves;
+}
+
+function generatePawnPushes(square) {
+    let possibleMoves = [];
+    
+    let pawnDirection = "";
+    
+    if (getColour(board[square]) == Colour.White) {
+        pawnDirection = "North";
+    } else if (getColour(board[square]) == Colour.Black) {
+        pawnDirection = "South";
+    }
+
+    if (board[square + DirectionOffset[pawnDirection]] != 0) {
+        return [];
+    } else {
+        possibleMoves.push({from: square, to: square + DirectionOffset[pawnDirection]});
+    }
+
+    let secondRank = 0;
+
+    if (getColour(board[square]) == Colour.White) {
+        secondRank = 6;
+    } else if (getColour(board[square]) == Colour.Black) {
+        secondRank = 1;
+    }
+
+    if (Math.floor(square / 8) == secondRank && board[square + 2 * DirectionOffset[pawnDirection]] == 0) {
+        possibleMoves.push({from: square, to: square + 2 * DirectionOffset[pawnDirection]});
+    }
+
+    return possibleMoves;
+}
+
+function generatePawnCaptures(square) {
+    let possibleMoves = [];
+    
+    let pawnDirection = "";
+    
+    let opposingColour = getColour(board[square]) == Colour.White ? Colour.Black : Colour.White;
+
+    if (getColour(board[square]) == Colour.White) {
+        pawnDirection = "North";
+    } else if (getColour(board[square]) == Colour.Black) {
+        pawnDirection = "South";
+    }
+
+    if (getColour(board[square + DirectionOffset[pawnDirection + "east"]]) == opposingColour) {
+        possibleMoves.push({from: square, to: square + DirectionOffset[pawnDirection + "east"]});
+    }
+
+    if (getColour(board[square + DirectionOffset[pawnDirection + "west"]]) == opposingColour) {
+        possibleMoves.push({from: square, to: square + DirectionOffset[pawnDirection + "west"]});
+    }
+
+    return possibleMoves;
+}
+
+function generatePawnMoves(square) {
+    return [...generatePawnPushes(square), ...generatePawnCaptures(square)];
+}
+
+function generateKnightMoves(square) {
+    let possibleMoves = [];
+
+    function directionComplement(direction) {
+        switch (direction) {
+            case Direction.North:
+            case Direction.South:
+                return [Direction.East, Direction.West];
+                break;
+            case Direction.East:
+            case Direction.West:
+                return [Direction.North, Direction.South];
+        }
+        return [];
+    }
+
+    for (var i = 0; i < 8; i += 2) {
+        for (var j = 0; j < directionComplement(i).length; j++) {
+            let minorDirection = direction[directionComplement(i)[j]];
+            let majorDirection = direction[i];
+            if  (squaresToEdge(square, Direction[minorDirection]) < 1 || squaresToEdge(square, Direction[majorDirection]) < 2 || getColour(board[square]) == getColour(board[square + 2 * DirectionOffset[majorDirection] + DirectionOffset[minorDirection]])) {} else {
+                possibleMoves.push({from: square, to: square + 2 * DirectionOffset[majorDirection] + DirectionOffset[minorDirection]});
+            }
+        }
+    }
+
+    return possibleMoves;
+}
+
+function isInCheck(colour) {
+
+}
+
+function move(from, to) {
+    let movedPiece = board[Square(from)];
+    let capturedPiece = board[Square(to)];
+    board[Square(to)] = movedPiece;
+    board[Square(from)] = Piece.None;
+    if (isInCheck(Colour.White)) {
+
+    }
+    updateBoard();
+    return capturedPiece;
 }
 
 function moves(colour) { // TODO: Non-sliding moves (King, Pawn, Knight)
     let allMoves = []
-    if (colour = Colour.White) {
+    if (colour == Colour.White) {
         let pieces = [];
-        for (var i = board.length - 1; i >= 0; i--) {
-            if (colour(board[i]) == Colour.White) pieces.push(i);
+        for (let i = 63; i >= 0; i--) {
+            if (getColour(board[i]) == Colour.White) pieces.push(i);
         }
 
-        for (var i = 0; i < pieces.length; i++) {
+        for (let i = 0; i < pieces.length; i++) {
             if (isSlidingPiece(board[pieces[i]])) {
-                allMoves = allmoves.concat(generateSlidingMoves(pieces[i], board[pieces[i]]));
+                allMoves = [...allMoves, ...generateSlidingMoves(pieces[i], board[pieces[i]])];
+            } else {
+                switch (board[pieces[i]]) {
+                    case Piece.whiteKing: allMoves = [...allMoves, ...generateKingMoves(pieces[i])]; break;
+                    case Piece.whitePawn: allMoves = [...allMoves, ...generatePawnMoves(pieces[i])]; break; 
+                    case Piece.whiteKnight: allMoves = [...allMoves, ...generateKnightMoves(pieces[i])]; break; 
+                }
             }
         }
-    } else if (colour = Colour.Black) {
+    } else if (colour == Colour.Black) {
+        let pieces = [];
+        for (let i = 63; i >= 0; i--) {
+            if (getColour(board[i]) == Colour.Black) pieces.push(i);
+        }
 
+        for (let i = 0; i < pieces.length; i++) {
+            if (isSlidingPiece(board[pieces[i]])) {
+                allMoves = [...allMoves, ...generateSlidingMoves(pieces[i], board[pieces[i]])];
+            } else {
+                switch (board[pieces[i]]) {
+                    case Piece.blackKing: allMoves = [...allMoves, ...generateKingMoves(pieces[i])]; break;
+                    case Piece.blackPawn: allMoves = [...allMoves, ...generatePawnMoves(pieces[i])]; break; 
+                    case Piece.blackKnight: allMoves = [...allMoves, ...generateKnightMoves(pieces[i])]; break; 
+                }
+            }
+        }
     }
     return allMoves;
 } 
